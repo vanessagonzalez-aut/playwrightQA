649dc0eaf1d9647945d61f3d3ae0246b81433928
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-04-28 14:45:39 -0600

    https://deploy-20260429--0c14ec0f.

--- tests/urls.js
+++ tests/urls.js
@@ -1,8 +1,8 @@
-const deploy_url = "https://avni-php-86b9hn34p--9755d960.visachinaonline.com/"
-const general_url = "https://avni-php-86b9hn34p--9755d960."  
-const email_test = "test123@mail.com"
-const requester = "Analia"
+const deploy_url = "https://deploy-20260429--0c14ec0f.visachinaonline.com/"
+const general_url = "https://deploy-20260429--0c14ec0f."  
+const email_test = "automations@ivisa.com"
+const requester = "Vanessa"
 let Orders = [] 
-module.exports = {deploy_url,  email_test, Orders, general_url}
+module.exports = {deploy_url,  email_test, Orders, general_url, requester}
 
     
