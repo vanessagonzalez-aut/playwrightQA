1759cb4d958b8323a962308e8a6252e888e199b6
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-04-30 10:14:09 -0600

    https://jamie-statsig-exposure--efa97b7e.

--- tests/urls.js
+++ tests/urls.js
@@ -1,7 +1,7 @@
-const deploy_url = "https://avni-php-86b9hn34p--d4d61481.visachinaonline.com/"
-const general_url = "https://avni-php-86b9hn34p--d4d61481."  
-const email_test = "qa12test@mail.com"
-const requester = "Analia"
+const deploy_url = "https://jamie-statsig-exposure--efa97b7e.visachinaonline.com/"
+const general_url = "https://jamie-statsig-exposure--efa97b7e."  
+const email_test = "test@test.com"
+const requester = "Maria"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
 
