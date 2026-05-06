f49547ffe25641890e68012b9ac2cef12f939017
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-05-06 09:44:37 -0600

    https://bryce-min-q-86b9t2n27--be08918d.

--- tests/urls.js
+++ tests/urls.js
@@ -1,5 +1,5 @@
-const deploy_url = "https://kenrick-payid-86b9t0fjv--6419b611.visachinaonline.com/"
-const general_url = "https://kenrick-payid-86b9t0fjv--6419b611."  
+const deploy_url = "https://bryce-min-q-86b9t2n27--be08918d.visachinaonline.com/"
+const general_url = "https://bryce-min-q-86b9t2n27--be08918d."  
 const email_test = "automations@ivisa.com"
 const requester = "Vanessa"
 let Orders = [] 
