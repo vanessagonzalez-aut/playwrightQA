e1cb8f16614b8d00442ad69e77fd1e779f248b79
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-05-18 11:38:23 -0600

    https://jamie-photo-services-1--fd9041c2.

--- tests/urls.js
+++ tests/urls.js
@@ -1,7 +1,7 @@
-const deploy_url = "https://deploy-20260518--f4c1c51d.visachinaonline.com/"
-const general_url = "https://deploy-20260518--f4c1c51d."  
-const email_test = "malamado@gmail.com"
-const requester = "Alejandro"
+const deploy_url = "https://jamie-photo-services-1--fd9041c2.visachinaonline.com/"
+const general_url = "https://jamie-photo-services-1--fd9041c2."  
+const email_test = "testing1@test.com"
+const requester = "Maria"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
 
