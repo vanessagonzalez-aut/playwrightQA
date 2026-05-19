243e63ca6b54313bf1de43c1ac1b410ae1ee30ac
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-05-19 14:50:22 -0600

    https://am-mcp-old-orders-86b9yu9eb--99f076be.

--- tests/urls.js
+++ tests/urls.js
@@ -1,7 +1,7 @@
-const deploy_url = "https://deploy-20260519--cbaf06c0.visachinaonline.com/"
-const general_url = "https://deploy-20260519--cbaf06c0."  
-const email_test = "camote@gmail.com"
-const requester = "Alejandro"
+const deploy_url = "https://am-mcp-old-orders-86b9yu9eb--99f076be.visachinaonline.com/"
+const general_url = "https://am-mcp-old-orders-86b9yu9eb--99f076be."  
+const email_test = "test1@test.com"
+const requester = "Maria"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
 
