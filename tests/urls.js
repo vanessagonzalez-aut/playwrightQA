2242caca57391d2750fca9b8b3090bce5917485a
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-05-21 11:22:33 -0600

    https://avni-mysql-86ba067qk--d1c70990.visachinaonline.com/

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = "https://deploy-20260521--ebb235ab.visachinaonline.com/"
-const general_url = "https://deploy-20260521--ebb235ab."  
-const email_test = "TEST111@test.com"
+const deploy_url = "https://avni-mysql-86ba067qk--d1c70990.visachinaonline.com/visachinaonline.com/"
+const general_url = "https://avni-mysql-86ba067qk--d1c70990.visachinaonline.com/"  
+const email_test = "test@test.com"
 const requester = "Maria"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
