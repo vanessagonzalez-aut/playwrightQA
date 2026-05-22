07090788282937c1424c5cc4e5558a0831b46448
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-05-22 07:49:23 -0600

    https://deploy-20260525--3a184416.

--- tests/urls.js
+++ tests/urls.js
@@ -1,5 +1,5 @@
-const deploy_url = "https://avni-mysql-86ba067qk--d1c70990.visachinaonline.com/visachinaonline.com/"
-const general_url = "https://avni-mysql-86ba067qk--d1c70990.visachinaonline.com/"  
+const deploy_url = "https://deploy-20260525--3a184416.visachinaonline.com/"
+const general_url = "https://deploy-20260525--3a184416."  
 const email_test = "test@test.com"
 const requester = "Maria"
 let Orders = [] 
