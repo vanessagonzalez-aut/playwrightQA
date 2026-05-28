cdf56e6cc29d62375002f254c8aed3b9416a2c71
vane0912 <vanessagpro5@gmail.com>
2026-05-28 11:31:08 -0600

    updates final

--- tests/Travel documents/subscription_individual.spec.js
+++ tests/Travel documents/subscription_individual.spec.js
@@ -1,36 +1,28 @@
 const { test, expect } = require('@playwright/test');
-const {general_url, deploy_url} = require('../urls');
+const percySnapshot = require('@percy/playwright');
 const appFunctions = require('../functions')
 const selectors = require('../selectors')
-const randomEmail = require('random-email')
-let wolfEmail = randomEmail({domain: "ivisatravel.com"})
+const {deploy_url} = require('../urls');
 
-test.describe.configure({ mode: 'serial' });
-test('File upload - Wolf', async({page, context}) =>{
+let Order_num 
+test('Individual subscription purchase', async ({ page }) => {
   test.slow()
-  await context.addCookies([
-    {
-      name: 'default_currency',
-      value: 'USD',
-      url: general_url + 'ivisatravel.visachinaonline.com'
-    }
-  ])
-  await page.goto(general_url + 'ivisatravel.visachinaonline.com/a/malaysia');
+  await page.goto(deploy_url + 'malaysia/apply-now')
   await page.getByRole("button").getByText("Add New Traveler").click()
   await appFunctions.step_1(page, "individual")
   const continue_sidebar = page.getByRole("button").getByText("Continue")
   await continue_sidebar.click()
-  await page.waitForURL("**/a/malaysia/apply-now/passport-details/0")
+  await page.waitForURL("**/malaysia/apply-now/passport-details/0")
   await appFunctions.step_2(page, continue_sidebar)
-  await page.waitForURL("**/a/malaysia/apply-now/address-details/0")
+  await page.waitForURL("**/malaysia/apply-now/address-details/0")
   await appFunctions.step_3c(page, continue_sidebar)
-  await page.waitForURL("**/a/malaysia/apply-now/additional-info/0")
+  await page.waitForURL("**/malaysia/apply-now/additional-info/0")
   await appFunctions.additionalInfo(page, continue_sidebar)
-  await page.waitForURL("**/a/malaysia/apply-now/traveler-review")
+  await page.waitForURL("**/malaysia/apply-now/traveler-review")
   await continue_sidebar.click()
-  await page.waitForURL("**/a/malaysia/apply-now/contact-details")
+  await page.waitForURL("**/malaysia/apply-now/contact-details")
   await continue_sidebar.click()
-  await page.waitForURL("**/a/malaysia/apply-now/checkout")
+  await page.waitForURL("**/malaysia/apply-now/checkout")
   await page.waitForTimeout(2000)
   const duplicate = await page.isVisible('id=btnDisclaimerNext')
   if (duplicate){
@@ -68,18 +60,18 @@ test('File upload - Wolf', async({page, context}) =>{
   await page.waitForTimeout(1000)
   await expect(next_btn).toBeEnabled()
   await next_btn.click()
-  await page.waitForURL(general_url + 'ivisatravel.visachinaonline.com/a/malaysia/order/' + Order_num + "/continue#step=trav0_passport_after_payment")
+  await page.waitForURL(deploy_url + "order/" + Order_num + "/continue#step=trav0_passport_after_payment")
   await selectors.inputText(page, "applicant.0.passport_num", "123456789")
   await selectors.datePicker(page, "applicant.0.passport_expiration_date", "9", "5", "2040")
   await page.locator("id=btnSubmitApplication").click()
-  await page.waitForURL(general_url + 'ivisatravel.visachinaonline.com/a/malaysia/' + "order-received-page/" + Order_num)
+  await page.waitForURL(deploy_url + "order-received-page/" + Order_num)
   await page.waitForTimeout(4000)
   const skip_recomendation = await page.locator('id=skip-recommendation-button').isVisible()
   if(skip_recomendation){
     await page.locator('id=skip-recommendation-button').click()    
   }
   await page.locator('id=trackApplication').click()  
-  await page.waitForURL(general_url + 'ivisatravel.visachinaonline.com/a/malaysia/order/' + Order_num)
+  await page.waitForURL(deploy_url + "order/" + Order_num)
   // Purchase subscription
 
   await page.getByText("View plans").click()
@@ -89,20 +81,20 @@ test('File upload - Wolf', async({page, context}) =>{
   await page.waitForTimeout(3000)
   await percySnapshot(page, 'Purchase Subscription modal');
   await page.getByTestId("purchase-subscription-button").click()
-  await page.waitForURL(general_url + 'ivisatravel.visachinaonline.com/a/malaysia/order/' + Order_num + "/purchase_addons/new_mop?subscription=true")
+  await page.waitForURL(deploy_url + "order/" + Order_num + "/purchase_addons/new_mop?subscription=true")
   await page.locator('id=cardNumber').frameLocator('[title="Card number"]').locator('id=primer-hosted-input').fill('4111 1111 1111 1111')
   await page.locator('id=expiry').frameLocator('[title="Expiry (MM/YY)"]').locator('id=primer-hosted-input').fill('10/26')
   await page.locator('id=cvv').frameLocator('[title="CVV"]').locator('id=primer-hosted-input').fill('123')
   await page.locator('id=cardFormName').frameLocator('[title="Name on card"]').locator('id=primer-hosted-input').fill('Jhon')
   await page.locator('id=btnSubmitPayment').click()
-  await page.waitForURL(general_url + 'ivisatravel.visachinaonline.com/a/malaysia/order/' + Order_num + "?subscription=true")
+  await page.waitForURL(deploy_url + "order/" + Order_num + "?subscription=true")
 
   // Place free order 
- await page.goto(general_url + 'ivisatravel.visachinaonline.com/a/malaysia/' + 'malaysia/apply-now')
+ await page.goto(deploy_url + 'malaysia/apply-now')
  await appFunctions.autofillExisting(page, "malaysia/apply-now/edit-traveler/0", false, true)
- await page.waitForURL("**/a/malaysia/apply-now/traveler-review")
+ await page.waitForURL("**/malaysia/apply-now/traveler-review")
  await continue_sidebar.click()
- await page.waitForURL("**/a/malaysia/apply-now/contact-details")
+ await page.waitForURL("**/malaysia/apply-now/contact-details")
   await continue_sidebar.click() 
   await page.waitForTimeout(2000)
   if (duplicate){
@@ -126,15 +118,15 @@ test('File upload - Wolf', async({page, context}) =>{
   await page.waitForTimeout(1000)
   await expect(next_btn).toBeEnabled()
   await next_btn.click()
-  await page.waitForURL(general_url + 'ivisatravel.visachinaonline.com/a/malaysia/order/' + Order_num + "/continue#step=trav0_passport_after_payment")
+  await page.waitForURL(deploy_url + "order/" + Order_num + "/continue#step=trav0_passport_after_payment")
   await selectors.inputText(page, "applicant.0.passport_num", "123456789")
   await selectors.datePicker(page, "applicant.0.passport_expiration_date", "9", "5", "2040")
   await page.locator("id=btnSubmitApplication").click()
-  await page.waitForURL(general_url + 'ivisatravel.visachinaonline.com/a/malaysia/' + "order-received-page/" + Order_num)
+  await page.waitForURL(deploy_url + "order-received-page/" + Order_num)
   await page.waitForTimeout(4000)
   if(skip_recomendation){
     await page.locator('id=skip-recommendation-button').click()    
   }
   await page.locator('id=trackApplication').click()  
-  await page.waitForURL(general_url + 'ivisatravel.visachinaonline.com/a/malaysia/order/' + Order_num)
+  await page.waitForURL(deploy_url + "order/" + Order_num)
 })
--- tests/urls.js
+++ tests/urls.js
@@ -1,7 +1,7 @@
 const deploy_url = "https://deploy-20260528--7bae7ba3.visachinaonline.com/"
 const general_url = "https://deploy-20260528--7bae7ba3."  
-const email_test = "automations@ivisa.com"
-const requester = "Alejandro"
+const email_test = "automations1@ivisa.com"
+const requester = "Vanessa"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
 
