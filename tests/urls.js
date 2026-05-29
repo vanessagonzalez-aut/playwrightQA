40149f0fd6d06449698100c9a813cd8af2e8fa61
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-05-29 07:30:34 -0600

    https://jamie-statsig-save-groups--5a78f42e.

--- tests/urls.js
+++ tests/urls.js
@@ -1,7 +1,7 @@
-const deploy_url = "https://deploy-20260528--7bae7ba3.visachinaonline.com/"
-const general_url = "https://deploy-20260528--7bae7ba3."  
-const email_test = "raro@gmail.com"
-const requester = "Alejandro"
+const deploy_url = "https://jamie-statsig-save-groups--5a78f42e.visachinaonline.com/"
+const general_url = "https://jamie-statsig-save-groups--5a78f42e."  
+const email_test = "test@test.com"
+const requester = "Maria"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
 
