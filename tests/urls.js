2952ad7170737d030e03c79ee1bed961b2d6f15d
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-06-02 16:48:28 -0600

    https://avni-composer-86ba8j138--8171ee7f.

--- tests/urls.js
+++ tests/urls.js
@@ -1,7 +1,7 @@
-const deploy_url = "https://deploy-20260603--8047789b.visachinaonline.com/"
-const general_url = "https://deploy-20260603--8047789b."  
-const email_test = "samsmith@gmail.com"
-const requester = "Alejandro"
+const deploy_url = "https://avni-composer-86ba8j138--8171ee7f.visachinaonline.com/"
+const general_url = "https://avni-composer-86ba8j138--8171ee7f."  
+const email_test = "automations@ivisa.com"
+const requester = "Vanessa"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
 
