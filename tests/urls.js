62c727a5bb8a9023117785baad9a0364b839c7a6
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-06-03 08:24:35 -0600

    https://da-doc-generate-validation--a3f6cb5e.

--- tests/urls.js
+++ tests/urls.js
@@ -1,7 +1,7 @@
-const deploy_url = "https://deploy-20260603--f0849175.visachinaonline.com/"
-const general_url = "https://deploy-20260603--f0849175."  
-const email_test = "camote@gmail.com"
-const requester = "Alejandro"
+const deploy_url = "https://da-doc-generate-validation--a3f6cb5e.visachinaonline.com/"
+const general_url = "https://da-doc-generate-validation--a3f6cb5e."  
+const email_test = "test1@test.com"
+const requester = "Maria"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
 
