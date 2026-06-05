db13d0897a1cffba833d0affd851b5c997936f1d
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-06-05 14:33:35 -0600

    https://kenrick-maestro-86ba5pef4--11ebf682.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
 const deploy_url = "https://kenrick-maestro-86ba5pef4--11ebf682.visachinaonline.com/"
 const general_url = "https://kenrick-maestro-86ba5pef4--11ebf682."  
-const email_test = "carol@payments.col"
+const email_test = "qatest@testing.col"
 const requester = "Carol"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
