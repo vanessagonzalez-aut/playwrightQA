a3bac63ffb77bf9688554e8df6d1279f3be9f5c0
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-06-12 13:41:20 -0600

    https://kenrick-fortercookie-86bad93k0--48ea49a5.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
 const deploy_url = "https://kenrick-fortercookie-86bad93k0--48ea49a5.visachinaonline.com/"
 const general_url = "https://kenrick-fortercookie-86bad93k0--48ea49a5."  
-const email_test = "test@mail.com"
+const email_test = "testqa@mailnator.com"
 const requester = "Analia"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
