99c8a1707510db21f6a5e7fde7b8019ed4bac95c
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-06-16 13:56:18 -0600

    https://andee-app-common-chunk-86baerbtx--5e04e5b7.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
 const deploy_url = "https://andee-app-common-chunk-86baerbtx--5e04e5b7.visachinaonline.com/"
 const general_url = "https://andee-app-common-chunk-86baerbtx--5e04e5b7."  
-const email_test = "carotest@prior.col"
+const email_test = "carol@qa.test"
 const requester = "Carol"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
