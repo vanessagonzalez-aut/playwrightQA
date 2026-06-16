46d87d370937b83602daf328e28d7607b552dcbc
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-06-16 17:47:39 -0600

    https://andee-vue-unused-components--3f838441.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = "https://andee-app-common-chunk-86baerbtx--5e04e5b7.visachinaonline.com/"
-const general_url = "https://andee-app-common-chunk-86baerbtx--5e04e5b7."  
-const email_test = "carol@qa.test"
+const deploy_url = "https://andee-vue-unused-components--3f838441.visachinaonline.com/"
+const general_url = "https://andee-vue-unused-components--3f838441."  
+const email_test = "testing@qa.test"
 const requester = "Carol"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
