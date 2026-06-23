84e92be2f14d13a2c18437491b414973b5a12e99
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-06-23 11:38:34 -0600

    https://kenrick-com-report-86bag5pqb--0c551c96.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = "https://deploy-20260623--67e5040f.visachinaonline.com/"
-const general_url = "https://deploy-20260623--67e5040f."  
-const email_test = "papa@gmail.com"
-const requester = "Alejandro"
+const deploy_url = "https://kenrick-com-report-86bag5pqb--0c551c96.visachinaonline.com/"
+const general_url = "https://kenrick-com-report-86bag5pqb--0c551c96."  
+const email_test = "juan@ceron.com"
+const requester = "Juan"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
