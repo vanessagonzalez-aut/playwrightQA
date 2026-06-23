6994b3bf0724dabdd4e639c8430160b239bcd6de
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-06-23 13:16:59 -0600

    https://deploy-20260624--d3c67611.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = "https://andee-eslint-suppressions-clear--33ca84b4.visachinaonline.com/"
-const general_url = "https://andee-eslint-suppressions-clear--33ca84b4."  
-const email_test = "qatest@testing.pl"
-const requester = "Carol"
+const deploy_url = "https://deploy-20260624--d3c67611.visachinaonline.com/"
+const general_url = "https://deploy-20260624--d3c67611."  
+const email_test = "malamado@gmail.com"
+const requester = "Alejandro"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
