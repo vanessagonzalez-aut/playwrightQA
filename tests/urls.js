a293dda97fec4386b933edac254a3fe1e579875e
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-06-24 12:48:49 -0600

    https://andee-prepay-aa-86ba65mmd--43cdec61.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = "https://deploy-20260625--b32f41a1.visachinaonline.com/"
-const general_url = "https://deploy-20260625--b32f41a1."  
-const email_test = "automations123@ivisa.com"
-const requester = "Vanessa"
+const deploy_url = "https://andee-prepay-aa-86ba65mmd--43cdec61.visachinaonline.com/"
+const general_url = "https://andee-prepay-aa-86ba65mmd--43cdec61."  
+const email_test = "test@test.com"
+const requester = "Maria"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
