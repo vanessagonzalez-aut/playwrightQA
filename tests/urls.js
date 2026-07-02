bbdd33b9528743399656dcc548d68540dca6a1f9
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-07-02 08:14:40 -0600

    https://deploy-20260702--50e471cb.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = "https://na-skip-indo-received--7ac02ed5.visachinaonline.com/"
-const general_url = "https://na-skip-indo-received--7ac02ed5."  
-const email_test = "test@automations.com"
-const requester = "Vanessa"
+const deploy_url = "https://deploy-20260702--50e471cb.visachinaonline.com/"
+const general_url = "https://deploy-20260702--50e471cb."  
+const email_test = "test@visatest.com"
+const requester = "Daniel"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
