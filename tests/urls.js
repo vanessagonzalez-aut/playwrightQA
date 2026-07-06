1bd4325b42d3e528db61cc9291be8592b0ae4cc9
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-07-06 04:20:45 -0600

    https://deploy-20260706--2baad946.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = "https://deploy-20260702--a3ae8ffd.visachinaonline.com/"
-const general_url = "https://deploy-20260702--a3ae8ffd."  
-const email_test = "fsdfd@automations.com"
-const requester = "Vanessa"
+const deploy_url = "https://deploy-20260706--2baad946.visachinaonline.com/"
+const general_url = "https://deploy-20260706--2baad946."  
+const email_test = "tessst@test.com"
+const requester = "Maria"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
