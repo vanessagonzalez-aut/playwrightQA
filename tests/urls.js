559c51ef1ab5304760e0ebffc1d667d60d14071a
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-07-07 14:01:57 -0600

    https://kenrick-ocr-reporting-86badzdu2--a611eda6.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = "https://ivisa.com/"
-const general_url = ""  
-const email_test = "sfhjkh@ivisa.com"
-const requester = "Vanessa"
+const deploy_url = "https://kenrick-ocr-reporting-86badzdu2--a611eda6.visachinaonline.com/"
+const general_url = "https://kenrick-ocr-reporting-86badzdu2--a611eda6."  
+const email_test = "test@test.com"
+const requester = "Maria"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
