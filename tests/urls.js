5f52e4be0541f1af0a183d8d58ee9871db40b3b4
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-07-15 08:43:24 -0600

    https://wog-eager-load-86bay0t03--c2bf121b.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = "https://carlos-css-order-details--ab339b82.visachinaonline.com/"
-const general_url = "https://carlos-css-order-details--ab339b82."  
-const email_test = "Test@testtt.com"
-const requester = "Maria"
+const deploy_url = "https://wog-eager-load-86bay0t03--c2bf121b.visachinaonline.com/"
+const general_url = "https://wog-eager-load-86bay0t03--c2bf121b."  
+const email_test = "test.a@mail.com"
+const requester = "Analia"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
