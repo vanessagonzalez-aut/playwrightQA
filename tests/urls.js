74b1eaa2717ee54fff8a62b8ec18c471f3fc1b28
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-07-17 12:29:20 -0600

    https://carlos-overflow-table--91d93f7b.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = "https://deploy-20260716--0c7d5983.visachinaonline.com/"
-const general_url = "https://deploy-20260716--0c7d5983."  
-const email_test = "automations@automations.com"
-const requester = "Vanessa"
+const deploy_url = "https://carlos-overflow-table--91d93f7b.visachinaonline.com/"
+const general_url = "https://carlos-overflow-table--91d93f7b."  
+const email_test = "testing@test.com"
+const requester = "Maria"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
