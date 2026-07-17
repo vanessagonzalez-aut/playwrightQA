837bba12f7b317dfce64df9db9cfa0f7a196237e
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-07-17 14:52:50 -0600

    https://carlos-overflow-table--91d93f7b.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
 const deploy_url = "https://carlos-overflow-table--91d93f7b.visachinaonline.com/"
 const general_url = "https://carlos-overflow-table--91d93f7b."  
-const email_test = "testing152@test.com"
+const email_test = "automations@automs.com"
 const requester = "Vanessa"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
