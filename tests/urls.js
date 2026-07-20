80642f8aa30e5d51517a7ff0e55ed9961b6c418f
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-07-20 13:25:14 -0600

    https://carlos-fix-dark-mode-bug--db819710.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = "https://aaron-es2020-86ba8hge5--5497b4ee.visachinaonline.com/"
-const general_url = "https://aaron-es2020-86ba8hge5--5497b4ee."  
-const email_test = "test123@test.com"
-const requester = "Daniel"
+const deploy_url = "https://carlos-fix-dark-mode-bug--db819710.visachinaonline.com/"
+const general_url = "https://carlos-fix-dark-mode-bug--db819710."  
+const email_test = "testttt@test.com"
+const requester = "Maria"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
