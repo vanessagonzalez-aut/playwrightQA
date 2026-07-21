d679444da5ef9f6df54f438dcd3235c47332cb74
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-07-21 11:31:00 -0600

    https://da-wog-tool-page-nav-bug--7e87d5ef.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = "https://deploy-20260722--044f1bcc.visachinaonline.com/"
-const general_url = "https://deploy-20260722--044f1bcc."  
-const email_test = "testtttt@test.com"
-const requester = "Maria"
+const deploy_url = "https://da-wog-tool-page-nav-bug--7e87d5ef.visachinaonline.com/"
+const general_url = "https://da-wog-tool-page-nav-bug--7e87d5ef."  
+const email_test = "juan@ceron.com"
+const requester = "Juan"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
