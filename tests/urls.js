8a55cc331ae38ab63b219789704a1c6d0d584fc9
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-07-29 16:34:31 -0600

    https://adam-mega-redir--f0243089.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = "https://deploy-20260730--134b9eb1.visachinaonline.com/"
-const general_url = "https://deploy-20260730--134b9eb1."  
-const email_test = "malamado@gmail.com"
-const requester = "Alejandro"
+const deploy_url = "https://adam-mega-redir--f0243089.visachinaonline.com/"
+const general_url = "https://adam-mega-redir--f0243089."  
+const email_test = "test2@qa.col"
+const requester = "Carol"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
