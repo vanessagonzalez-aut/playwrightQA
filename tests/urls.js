c6b81d0775b6da5df1d4a219c588fbc99439039b
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-08-03 09:08:09 -0600

    https://da-keep-annotations-changes--d68b539b.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = "https://deploy-20260803--7081a758.visachinaonline.com/"
-const general_url = "https://deploy-20260803--7081a758."  
-const email_test = "testtttttting@test.com"
+const deploy_url = "https://da-keep-annotations-changes--d68b539b.visachinaonline.com/"
+const general_url = "https://da-keep-annotations-changes--d68b539b."  
+const email_test = "test@test.com"
 const requester = "Maria"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
