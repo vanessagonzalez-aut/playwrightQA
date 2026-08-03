c6e0e2b0f0f2aecd4450d87bf6325546b10e6ac9
vanessagonzalez-aut <vanessa.gonzalez@ivisa.com>
2026-08-03 12:01:27 -0600

    https://na-admin-del-passport--05d0963f.

--- tests/urls.js
+++ tests/urls.js
@@ -1,6 +1,6 @@
-const deploy_url = "https://da-keep-annotations-changes--d68b539b.visachinaonline.com/"
-const general_url = "https://da-keep-annotations-changes--d68b539b."  
-const email_test = "test@test.com"
-const requester = "Maria"
+const deploy_url = "https://na-admin-del-passport--05d0963f.visachinaonline.com/"
+const general_url = "https://na-admin-del-passport--05d0963f."  
+const email_test = "automations123@ivisa.com"
+const requester = "Vanessa"
 let Orders = [] 
 module.exports = {deploy_url,  email_test, Orders, general_url, requester}
